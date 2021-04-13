49e71c760556ab917e497dc4a03b9951508fb78e
miyu0201 <y.minghui@yahoo.com>
2021-04-13 15:01:18 +0200

    edit traffic control

--- Assets/trafficControl.cs
+++ Assets/trafficControl.cs
@@ -5,30 +5,54 @@ using UnityEngine.AI;
 
 public class trafficControl : MonoBehaviour
 {
-    public GameObject lightA;
-    
-    
+    public GameObject[] light;
+    public GameObject[] obstacle;
+
     void Start()
     {
-        UnityEngine.AI.NavMeshObstacle obstacle = GetComponent<UnityEngine.AI.NavMeshObstacle>();
-        obstacle.gameObject.SetActive(true);
+        //UnityEngine.AI.NavMeshObstacle obstacle = GetComponent<UnityEngine.AI.NavMeshObstacle>();
+        // obstacle.gameObject.SetActive(true);
+
     }
-   void Update()
+    void Update()
     {
-        UnityEngine.AI.NavMeshObstacle obstacle = GetComponent<UnityEngine.AI.NavMeshObstacle>();
-        
-            if(lightA.GetComponent<Renderer>().material.color == Color.red)
+        // UnityEngine.AI.NavMeshObstacle obstacle = GetComponent<UnityEngine.AI.NavMeshObstacle>();
+        for (int i = 0; i < light.Length; i++)
+        {
+            if (light[i].GetComponent<Renderer>().material.color == Color.red)
             {
-                obstacle.enabled = true;
+                stop();
                 Debug.Log("red");
             }
-       
-       else if (lightA.GetComponent<Renderer>().material.color == Color.green)
-           {
-            obstacle.enabled = false;
-            Debug.Log("green");
-           } 
+
+            // if(light.GetComponent<Renderer>().material.color == Color.green)
+            else
+            {
+                go();
+                Debug.Log("green");
+            }
+        }
     }
+    void stop()
+    {
+        //UnityEngine.AI.NavMeshObstacle obstacle = GetComponent<UnityEngine.AI.NavMeshObstacle>();
+        for (int i = 0; i < obstacle.Length; i++)
+        {
+            obstacle[i].gameObject.SetActive(true);
+        }
+
+    }
+
+    void go()
+    {
+        //UnityEngine.AI.NavMeshObstacle obstacle = GetComponent<UnityEngine.AI.NavMeshObstacle>();
+        for (int i = 0; i < obstacle.Length; i++)
+        {
+            obstacle[i].gameObject.SetActive(false);
+        }
+    }
+
+
 
 
 }
