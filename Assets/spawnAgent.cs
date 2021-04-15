4f49361da447dfa79df7d5ec7782711a484725ed
Yoghurt Smak av jordgubbe <yazzanderssonlinderholm@hotmail.com>
2021-04-15 15:39:53 +0200

    spawnAgent updated

--- Assets/spawnAgent.cs
+++ Assets/spawnAgent.cs
@@ -4,26 +4,33 @@ using UnityEngine;
 
 public class spawnAgent : MonoBehaviour
 {
-    public GameObject agent;
-    public GameObject goalObject;
+    public GameObject agent1;
+    public GameObject agent2;
+    public GameObject[] allGoals;
 
     // Start is called before the first frame update
     void Start()
     {
-       Invoke("Spawn", 2); //call Spawnfunction after 2 seconds
+        Invoke("Spawn", (Random.Range(0, 10))); //call Spawnfunction after 2 seconds
     }
 
     // Update is called once per frame
    void Spawn()
     {
-      GameObject a = (GameObject) Instantiate(agent, this.transform.position, Quaternion.identity); //instantiate theh copy of vehical and store in varibal v
-      a.GetComponent<driveTo>().goal = goalObject.transform;//get hold of the goal in DriveTo script, andn set it to be the transform of goalObject
-      //Invoke("Spawn", Random.Range(2,5);
-      //Invoke("Spawn", 30);
+        GameObject[] allAgents = { agent1, agent2 };
+        int random_num = Random.Range(0, allAgents.Length);
+        GameObject agent = allAgents[random_num];
+        GameObject a = (GameObject)Instantiate(agent, this.transform.position, Quaternion.identity); //instantiate theh copy of vehical and store in varibal v
+
+        int rand = Random.Range(0, allGoals.Length);
+        GameObject goalObject = allGoals[rand];
+        a.GetComponent<driveTo>().goal = goalObject.transform;//get hold of the goal in DriveTo script, andn set it to be the transform of goalObject
+        //Invoke("Spawn", Random.Range(2,5);
+        //Invoke("Spawn", 30);
     }
 
     void Update()
-   {
+    {
        
        
     }
