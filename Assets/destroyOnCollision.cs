b5977cd3f90cfaf7f43c7177b853e04a1c9286fd
Yoghurt Smak av jordgubbe <yazzanderssonlinderholm@hotmail.com>
2021-04-15 15:42:36 +0200

    destroyOnCollision updated

--- Assets/destroyOnCollision.cs
+++ Assets/destroyOnCollision.cs
@@ -6,22 +6,21 @@ public class destroyOnCollision : MonoBehaviour
 {
     public GameObject agent;
     public GameObject goalObject;
-    public GameObject spawn;
+    public GameObject[] allSpawnPoints;
 
     void OnCollisionEnter(Collision collision)
     {
-      
-            collision.gameObject.SetActive(false);
-            Invoke("ReSpawn", 2);
-            Destroy(collision.gameObject); //after reSpawn, then is able to destory the old gameObject clone of Vehicle
-
+        collision.gameObject.SetActive(false);
+        Invoke("ReSpawn", 2);
+        Destroy(collision.gameObject); //after reSpawn, then is able to destory the old gameObject clone of Vehicle
     }
 
     void ReSpawn()
     {
+        int rand = Random.Range(0, allSpawnPoints.Length);
+        GameObject spawn = allSpawnPoints[rand];
         GameObject a = (GameObject)Instantiate(agent, spawn.transform.position, Quaternion.identity); //instantiate theh copy of vehical and store in varibal v
         a.GetComponent<driveTo>().goal = goalObject.transform;//get hold of the goal in DriveTo script, andn set it tobe the transform of goalObject
-        
     }
 
 
