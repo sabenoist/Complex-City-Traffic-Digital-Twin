3abeacc9fa91ea43cd102cb104579139dfd5efbb
Yoghurt Smak av jordgubbe <yazzanderssonlinderholm@hotmail.com>
2021-05-03 15:13:42 +0200

    yazz buss two goal code added

---
+++ Assets/Scripts/changeDestination.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeDestination : MonoBehaviour
+{
+    // the bus prefab needs to be dragged into this slot in the Inspector
+    public driveTo dT;
+
+    void OnCollisionEnter(Collision collision)
+    //void OnTriggerEnter(Collider collider)
+    {
+        //collision.gameObject.SetActive(false);
+        Debug.Log("collision");
+        dT.changeGoal(); // this line calls the function in the 'driveTo'-script
+    }
+
+}
--- Assets/Scripts/driveTo.cs
+++ Assets/Scripts/driveTo.cs
@@ -19,3 +19,48 @@ public class driveTo : MonoBehaviour
 
     }
 }
+
+/* THIS IS THE WORK YAZZ DID TRYING TO MAKE THE BUS HAVE TWO DESTINATIONS
+ * 
+ * 
+ * 
+public class driveTo : MonoBehaviour
+{
+    // this is the old code
+    //public Transform goal;
+
+    // this is the new code, allowing for two goals for all vehicles
+    public Transform goal_1;
+    public Transform goal_2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // this is the old code
+        //UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        //agent.destination = goal.position;
+
+
+        // this is the new code, making the first goal the goal of all vehicles
+        UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.destination = goal_1.position;
+    }
+
+    // this is part of the new code,
+    // a function changing the goal of the bus
+    // this is called from the 'changeDestination'-script that is actiated at the first goal of the bus
+    public void changeGoal()
+    {
+        Debug.Log("driveTo scrpit called!");
+
+        UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.destination = goal_2.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}*/
