ec0fb891919045f43c565b200e1bd34eb3b793f6
Yoghurt Smak av jordgubbe <yazzanderssonlinderholm@hotmail.com>
2021-05-03 15:45:32 +0200

    commented out all of 'changeDestination'-sript

--- Assets/Scripts/changeDestination.cs
+++ Assets/Scripts/changeDestination.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class changeDestination : MonoBehaviour
 {
     // the bus prefab needs to be dragged into this slot in the Inspector
-    public driveTo dT;
+    //public driveTo dT;
 
-    void OnCollisionEnter(Collision collision)
+    //void OnCollisionEnter(Collision collision)
     //void OnTriggerEnter(Collider collider)
     {
         //collision.gameObject.SetActive(false);
-        Debug.Log("collision");
-        dT.changeGoal(); // this line calls the function in the 'driveTo'-script
-    }
+        //Debug.Log("collision");
+        //dT.changeGoal(); // this line calls the function in the 'driveTo'-script
+    //}
 
 }
