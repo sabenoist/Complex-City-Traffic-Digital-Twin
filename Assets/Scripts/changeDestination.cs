d34210825e006315cbae8fe6c97c55ea6ccbcad6
Sander Arjan Benoist <sabenoist5@gmail.com>
2021-05-16 12:38:48 +0200

    small fix for the bus driver interrupt system

--- Assets/Scripts/changeDestination.cs
+++ Assets/Scripts/changeDestination.cs
@@ -4,26 +4,17 @@ using UnityEngine;
 
 public class changeDestination : MonoBehaviour
 {
-    // the bus prefab needs to be dragged into this slot in the Inspector
-    //public driveToBus dT;
     public GameObject ownObject;
 
     void OnCollisionEnter(Collision collision)
     //void OnTriggerEnter(Collider collider)
     {
-        //collision.gameObject.SetActive(false);
         Debug.LogWarning("collision");
         
         GameObject bus = collision.gameObject;
-        //bus.GetComponent<driveToBus>().stopBus();
         bus.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-        //busControl(bus);
 
         StartCoroutine(busControl(bus));
-
-       // dT.changeGoal(); // this line calls the function in the 'driveTo'-script
-
-        //ownObject.GetComponent<BoxCollider>().enabled = false;
     }
 
     IEnumerator busControl(GameObject bus) {
@@ -32,10 +23,14 @@ public class changeDestination : MonoBehaviour
         if (bus.GetComponent<driveToBus>().waitLonger) {
             yield return new WaitForSeconds(20);
         }
+        
         bus.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
         bus.GetComponent<driveToBus>().changeGoal();
+        bus.GetComponent<driveToBus>().waitLonger = false;
+
         ownObject.GetComponent<BoxCollider>().enabled = false;
         yield return new WaitForSeconds(5);
         ownObject.GetComponent<BoxCollider>().enabled = true;
+
     }
 }
